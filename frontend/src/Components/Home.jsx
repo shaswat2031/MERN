27fc1d3539a303b270b7f59eafdbe1b7a535d6b9
shaswat2031 <prasadshaswat9265@gmail.com>
2025-02-18 09:50:34 +0530

    fix: update resume download link to use direct access URL

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -115,7 +115,7 @@ const Home = () => {
           transition={{ delay: 0.8, duration: 0.6, ease: "easeInOut" }}
         >
           <a
-            href="https://drive.google.com/file/d/1clis7nzq4Enrdn9PcVZfzEwUUVa3jyKi/view?usp=sharing"
+            href="https://drive.google.com/uc?id=1clis7nzq4Enrdn9PcVZfzEwUUVa3jyKi"
             download="Prasadshaswatresume.pdf"
             className="px-6 py-3 text-white bg-blue-600 rounded-lg shadow-md hover:bg-blue-700 transition duration-300"
           >
